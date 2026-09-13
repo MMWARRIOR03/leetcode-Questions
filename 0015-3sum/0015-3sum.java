f0d5fca471eb94c8c16a4882a310f2af21f5d7e6
Mrinal Mohit <154472831+MMWARRIOR03@users.noreply.github.com>
2026-09-13 14:54:31 +0530

    Time: 34 ms (57.81%), Space: 59 MB (74.48%) - LeetHub

---
+++ 0015-3sum/0015-3sum.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res=new ArrayList<>();
+
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length-2;i++){
+            // for(int j=i+1;j<nums.length-1;j++){
+            //     for(int k=j+1;k<nums.length;k++){
+            //         if(nums[i]+nums[j]+nums[k]==0){
+            //             List<Integer> sum=new ArrayList<>();
+            //             sum.add(nums[i]);
+            //             sum.add(nums[j]);
+            //             sum.add(nums[k]);
+            //             res.add(sum);
+            //         }
+            //     }
+            // }
+            if(i>0&&nums[i]==nums[i-1]){
+                continue;
+            }
+            int j=i+1;
+            int k=nums.length-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    res.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    while(j<k&&nums[j]==nums[j+1]){
+                        j++;
+                    }
+                    while(j<k&&nums[k]==nums[k-1]){
+                        k--;
+                    }
+                    j++;
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+        }
+        // List<List<Integer>> result=new ArrayList<>();
+        // Set<List<Integer>> set=new HashSet<>();
+        // for(List<Integer> li:res){
+        //     if(set.add(li)==true){
+        //         result.add(li);
+        //     }
+        // }
+        return res;
+    }
+}
